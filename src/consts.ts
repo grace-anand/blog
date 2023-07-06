480959477b9c0116b915985b3268bc0119e67ee2
Anand R <graceanand99@gmail.com>
2023-07-06 23:48:19 +0530

    Organised overall app's folder structure

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,20 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
 export const SITE_TITLE = 'My personal website.';
 export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const LINKS = [
+	{
+		href: '/',
+		text: 'Home',
+	},
+	{
+		href: '/blog',
+		text: 'Blog',
+	},
+	{
+		href: '/#about',
+		text: 'About',
+	},
+	{
+		href: '/#projects',
+		text: 'Projects',
+	},
+];
