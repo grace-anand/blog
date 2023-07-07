1b4f41c5957e00a7955ef8767123f3ebca4ce32f
Anand R <graceanand99@gmail.com>
2023-07-07 23:57:50 +0530

    Changed the theme to use from manual theme

--- astro.config.ts
+++ astro.config.ts
@@ -3,13 +3,12 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
+import { theme } from './syntax-highlighting-theme';
 
 export default defineConfig({
 	site: 'https://graceanand.dev',
 	integrations: [mdx(), sitemap(), react(), tailwind()],
 	markdown: {
-		shikiConfig: {
-			theme: 'one-dark-pro',
-		},
+		shikiConfig: { theme },
 	},
 });
