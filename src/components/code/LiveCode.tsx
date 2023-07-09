072f81df0a7a84b43d962e9598de68c3b118bfcc
Anand R <graceanand99@gmail.com>
2023-07-09 23:59:32 +0530

    Added Sandpack

---
+++ src/components/code/LiveCode.tsx
@@ -0,0 +1,8 @@
+import { Sandpack } from '@codesandbox/sandpack-react';
+import { nightOwl } from '@codesandbox/sandpack-themes';
+
+const LiveCode = () => {
+	return <Sandpack themes={[nightOwl]} template="react" />;
+};
+
+export default LiveCode;
