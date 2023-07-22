3e92e0ebddcfa194adc2b38dcc4d48e305a3fbb9
Anand R <graceanand99@gmail.com>
2023-07-22 23:52:00 +0530

    Added circular motion bg style

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -72,10 +72,28 @@ export default {
 					from: { height: 'var(--radix-accordion-content-height)' },
 					to: { height: '0' },
 				},
+				circular: {
+					'0%': {
+						transform: 'rotate(0deg) translate(190px) rotate(0deg)',
+					},
+					'100%': {
+						transform: 'rotate(360deg) translate(190px) rotate(-360deg)',
+					},
+				},
+				'circular-reverse': {
+					'0%': {
+						transform: 'rotate(0deg) translate(190px) rotate(0deg)',
+					},
+					'100%': {
+						transform: 'rotate(-360deg) translate(190px) rotate(360deg)',
+					},
+				},
 			},
 			animation: {
 				'accordion-down': 'accordion-down 0.2s ease-out',
 				'accordion-up': 'accordion-up 0.2s ease-out',
+				circular: 'circular 10s infinite linear',
+				'circular-reverse': 'circular-reverse 10s infinite linear',
 			},
 		},
 	},
