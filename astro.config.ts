55b2e2f8545bd270c78fd11b675e1b9c946e07b5
Anand R <graceanand99@gmail.com>
2023-07-30 00:39:39 +0530

    Added rehype pretty code and removed code title package

--- astro.config.ts
+++ astro.config.ts
@@ -3,13 +3,18 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
-import remarkCodeTitles from 'remark-code-titles';
+import rehypePrettyCode from 'rehype-pretty-code';
+
+const prettyCodeOptions = {
+	theme: 'one-dark-pro',
+};
 
 export default defineConfig({
 	site: 'https://graceanand.dev',
 	integrations: [mdx(), sitemap(), react(), tailwind()],
 	markdown: {
-		remarkPlugins: [remarkCodeTitles],
+		syntaxHighlight: false,
+		rehypePlugins: [[rehypePrettyCode, prettyCodeOptions]],
 		shikiConfig: { theme: 'one-dark-pro' },
 	},
 	experimental: {
