067a11fa16bbeb4ed89982dee238909d8b26927b
Anand R <graceanand99@gmail.com>
2024-03-03 22:55:29 +0530

    Fix color issue on detail tag

--- src/components/navigation/right-sidebar/TableOfContents.tsx
+++ src/components/navigation/right-sidebar/TableOfContents.tsx
@@ -41,12 +41,12 @@ const TableOfContents = ({ toc = [], labels, isMobile }: Props) => {
 			<summary>
 				<div className="inline-block">{children}</div>
 				{!open && currentHeading?.slug !== 'overview' && (
-					<span className="text-foreground pb-1">
+					<span className="text-white pb-1">
 						<svg
 							xmlns="http://www.w3.org/2000/svg"
 							viewBox="0 1 16 16"
 							aria-hidden="true"
-							className="fill-foreground w-4 h-4 inline-block"
+							className="fill-white w-4 h-4 inline-block"
 						>
 							<path
 								fillRule="evenodd"
